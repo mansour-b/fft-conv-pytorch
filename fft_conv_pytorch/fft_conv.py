3989648e883fc5c8715869a66f3f27e27208fc2d
Mansour Benbakoura <mansour.benbakoura@inria.fr>
2025-03-17 16:44:19 +0100

    wip function fft conv transpose

--- fft_conv_pytorch/fft_conv.py
+++ fft_conv_pytorch/fft_conv.py
@@ -157,6 +157,105 @@ def fft_conv(
 
     return output
 
+def fft_conv_transpose(
+    signal: Tensor,
+    kernel: Tensor,
+    bias: Tensor = None,
+    padding: Union[int, Iterable[int], str] = 0,
+    padding_mode: str = "constant",
+    stride: Union[int, Iterable[int]] = 1,
+    dilation: Union[int, Iterable[int]] = 1,
+    groups: int = 1,
+) -> Tensor:
+    """Performs N-d convolution of Tensors using a fast fourier transform, which
+    is very fast for large kernel sizes. Also, optionally adds a bias Tensor
+    after the convolution (in order ot mimic the PyTorch direct convolution).
+
+    Args:
+        signal: (Tensor) Input tensor to be convolved with the kernel.
+        kernel: (Tensor) Convolution kernel.
+        bias: (Tensor) Bias tensor to add to the output.
+        padding: (Union[int, Iterable[int], str) If int, Number of zero samples
+            to pad then input on the last dimension. If str, "same" supported
+            to pad input for size preservation.
+        padding_mode: (str) Padding mode to use from {constant, reflection,
+            replication}. reflection not available for 3d.
+        stride: (Union[int, Iterable[int]) Stride size for computing
+            output values.
+        dilation: (Union[int, Iterable[int]) Dilation rate for the kernel.
+        groups: (int) Number of groups for the convolution.
+
+    Returns:
+        (Tensor) Convolved tensor
+    """
+
+    # Cast padding, stride & dilation to tuples.
+    n = signal.ndim - 2
+    stride_ = to_ntuple(stride, n=n)
+    dilation_ = to_ntuple(dilation, n=n)
+    if isinstance(padding, str):
+        if padding == "same":
+            if stride != 1 or dilation != 1:
+                raise ValueError("stride must be 1 for padding='same'.")
+            padding_ = [(k - 1) / 2 for k in kernel.shape[2:]]
+        else:
+            raise ValueError(f"Padding mode {padding} not supported.")
+    else:
+        padding_ = to_ntuple(padding, n=n)
+
+    # internal dilation offsets
+    offset = torch.zeros(
+        1, 1, *dilation_, device=signal.device, dtype=signal.dtype
+    )
+    offset[(slice(None), slice(None), *((0,) * n))] = 1.0
+
+    # correct the kernel by cutting off unwanted dilation trailing zeros
+    cutoff = tuple(slice(None, -d + 1 if d != 1 else None) for d in dilation_)
+
+    # pad the kernel internally according to the dilation parameters
+    kernel = torch.kron(kernel, offset)[(slice(None), slice(None)) + cutoff]
+
+    # Pad the input signal & kernel tensors (round to support even sized
+    # convolutions)
+    signal_padding = [r(p) for p in padding_[::-1] for r in (floor, ceil)]
+    signal = f.pad(signal, signal_padding, mode=padding_mode)
+
+    # Because PyTorch computes a *one-sided* FFT, we need the final dimension to
+    # have *even* length.  Just pad with one more zero if the final dimension is
+    # odd.
+    signal_size = signal.size()  # original signal size without padding to even
+    if signal.size(-1) % 2 != 0:
+        signal = f.pad(signal, [0, 1])
+
+    kernel_padding = [
+        pad
+        for i in reversed(range(2, signal.ndim))
+        for pad in [0, signal.size(i) - kernel.size(i)]
+    ]
+    padded_kernel = f.pad(kernel, kernel_padding)
+
+    # Perform fourier convolution -- FFT, matrix multiply, then IFFT
+    signal_fr = rfftn(signal.float(), dim=tuple(range(2, signal.ndim)))
+    kernel_fr = rfftn(padded_kernel.float(), dim=tuple(range(2, signal.ndim)))
+
+    kernel_fr.imag *= -1
+    output_fr = complex_matmul(signal_fr, kernel_fr, groups=groups)
+    output = irfftn(output_fr, dim=tuple(range(2, signal.ndim)))
+
+    # Remove extra padded values
+    crop_slices = [slice(None), slice(None)] + [
+        slice(0, (signal_size[i] - kernel.size(i) + 1), stride_[i - 2])
+        for i in range(2, signal.ndim)
+    ]
+    output = output[crop_slices].contiguous()
+
+    # Optionally, add a bias term before returning.
+    if bias is not None:
+        bias_shape = tuple([1, -1] + (signal.ndim - 2) * [1])
+        output += bias.view(bias_shape)
+
+    return output
+
 
 class _FFTConv(nn.Module):
     """Base class for PyTorch FFT convolution layers."""
